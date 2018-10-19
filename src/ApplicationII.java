5dde9e5e94785bfa2018e2f573205304f3d8a810
B0BAI <git@bobaikato.io>
2018-10-19 16:18:33 +0100

    Working state of application

--- src/ApplicationII.java
+++ src/ApplicationII.java
@@ -6,9 +6,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ApplicationII {
     private static Map<Integer, String> sentenceMap = new HashMap<>();
     private static List<String> sentenceList;
-    private static List<String> newSentenceList;
 
-    private static String TARGET_WORD = "AND";
+    private static String TARGET_WORD = "BOBAI";
 
     private static Runnable convertSentenceToList(String sentence) {
         return () -> sentenceList = new ArrayList<String>(Stream.of(sentence.split(" "))
@@ -24,15 +23,15 @@ public class ApplicationII {
     }
 
     private static Runnable tagTargetWord(int key, String value) {
+
         return () -> sentenceMap.put(key, "<b>" + value + "</b>");
     }
 
-    private Runnable processTagging(Map<Integer, String> sentenceMap) {
+    private static Runnable processTagging(Map<Integer, String> sentenceMap) {
         return () -> {
-
             Map<Integer, String> taggingresult = sentenceMap.entrySet()
                     .stream()
-                    .filter(map -> "Orange".equalsIgnoreCase(map.getValue()))
+                    .filter(map -> TARGET_WORD.equalsIgnoreCase(map.getValue()))
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
             taggingresult.entrySet()
@@ -45,10 +44,11 @@ public class ApplicationII {
 
     public static void main(String[] args) {
 
-        String TARGET_SENTENCE = "Composing small pieces of functionality relies on repeatable results, a standard mechanism for input and output, and an exit code for a program to indicate success or lack thereof.  We know this works from observational evidence, and we also know that a Unix pipe is a “dumb” interface because it has no control statements.  The pipe applies SRP by pushing data from A to B, and it’s up to members of the pipeline to decide if the input is unacceptable.";
+        String TARGET_SENTENCE = "Bobai small pieces of functionality relies Bobai repeatable results, a standard mechanism for input and output, and an exit code for a program to indicate success or lack thereof.  We know this works from Bobai evidence The Bobai applies SRP by Bobai data from A to B, and it’s up to members of the pipeline to Bobai if the input is Bobai.";
 
         convertSentenceToList(TARGET_SENTENCE).run();
         convertSentenceListToMap(sentenceList).run();
+        processTagging(sentenceMap).run();
 
 
         System.out.println(sentenceMap);
