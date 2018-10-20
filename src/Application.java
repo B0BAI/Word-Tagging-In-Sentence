670cae2f00cf39e6f689c48c9c74f277226693e4
B0BAI <git@bobaikato.io>
2018-10-20 12:46:10 +0100

    Extinct all thread — final state 🤘🏿

--- src/Application.java
+++ src/Application.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+import java.time.Instant;
 import java.util.stream.*;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -20,28 +22,24 @@ public class Application {
                 .collect(Collectors.toList()));
     }
 
-    private static Runnable convertSentenceListToMap(List<String> list) {
-        return () -> {
-            AtomicInteger index = new AtomicInteger(-1);
-            for (String item : list) sentenceMap.put(index.incrementAndGet(), item);
-        };
+    private static void convertSentenceListToMap(List<String> list) {
+        AtomicInteger index = new AtomicInteger(-1);
+        for (String item : list) sentenceMap.put(index.incrementAndGet(), item);
     }
 
     private static Runnable tagTargetWord(int key, String value) {
         return () -> sentenceMap.put(key, "<b>" + value + "</b>");
     }
 
-    private static Runnable processTagging(Map<Integer, String> sentenceMap) {
-        return () -> {
-            Map<Integer, String> taggingResult = sentenceMap.entrySet()
-                    .stream()
-                    .filter(map -> TARGET_WORD.equalsIgnoreCase(map.getValue().replaceAll("[^a-zA-Z0-9]", "")))
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    private static void processTagging(Map<Integer, String> sentenceMap) {
+        Map<Integer, String> taggingResult = sentenceMap.entrySet()
+                .stream()
+                .filter(map -> TARGET_WORD.equalsIgnoreCase(map.getValue().replaceAll("[^a-zA-Z0-9]", "")))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
-            taggingResult.entrySet()
-                    .parallelStream()
-                    .forEach(entry -> tagTargetWord(entry.getKey(), entry.getValue()).run());
-        };
+        taggingResult.entrySet()
+                .parallelStream()
+                .forEach(entry -> tagTargetWord(entry.getKey(), entry.getValue()).run());
     }
 
     private static void getTextFileContent(String filePath) {
@@ -59,22 +57,25 @@ public class Application {
                 .collect(Collectors.joining(" "));
     }
 
-    private static Runnable writeOutputToFile(String output){
-        return ()->{
-            try {
-                Files.write(Paths.get("output.txt"), output.getBytes());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            System.out.println("DONE!");
-        };
+    private static void writeOutputToFile(String output) {
+        try {
+            Files.write(Paths.get("output.txt"), output.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        getTextFileContent("sentence.txt");
+        Instant start = Instant.now();
 
-        convertSentenceListToMap(sentenceList).run();
-        processTagging(sentenceMap).run();
-        writeOutputToFile(convertSentenceMapToString(sentenceMap)).run();
+        getTextFileContent("/Users/B0BAI/W O R K S P A C E/Word-Tagging-In-Sentence/src/sentence.txt");
+
+        convertSentenceListToMap(sentenceList);
+        processTagging(sentenceMap);
+        writeOutputToFile(convertSentenceMapToString(sentenceMap));
+
+        Instant end = Instant.now();
+        System.out.println(Duration.between(start, end).getNano());
+        //System.out.println(Runtime.getRuntime().freeMemory());
     }
 }
