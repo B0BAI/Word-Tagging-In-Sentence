420d59b200251db4bcecc6c2081e16dc3498467a
B0BAI <git@bobaikato.io>
2018-10-20 13:41:33 +0100

    Renaming

--- src/Application.java
+++ src/Application.java
@@ -4,7 +4,7 @@ import java.time.Instant;
 
 public class Application implements Tagging {
 
-    private static Application setFilePath(String filePath) {
+    private static Application setContentFilePath(String filePath) {
         Application application = new Application();
         application.loadFileContent(filePath);
         return application;
@@ -14,7 +14,7 @@ public class Application implements Tagging {
     public static void main(String[] args) {
         Instant start = Instant.now();
 
-        Application.setFilePath("sentence.txt").tag("expansion");
+        Application.setContentFilePath("sentence.txt").tag("expansion");
 
         Instant end = Instant.now();
         System.out.println(Duration.between(start, end).getNano());
